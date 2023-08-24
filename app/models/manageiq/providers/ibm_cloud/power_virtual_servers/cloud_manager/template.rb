28c5bfe8dbb9d81ab2195b87561b5d852b346585
Jay Carman <jwcarman@us.ibm.com>
2023-08-24 11:17:21 -0500

    Override number_of_cpus_for_request
    PowerVS uses the more typical Flavor style of cpu allocation for SAP
    profiles. For non SAP profiles, the Flavor only set the host type and
    the user is allowed to request any number of cpus using the
    entitled_processors form option.

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/template.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/template.rb
@@ -59,6 +59,15 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::Template
     _log.error("image=[#{name}], error: #{e}")
   end
 
+  def number_of_cpus_for_request(request)
+    flavor_obj = Flavor.find_by(:name => request.options[:sys_type][1])
+    if flavor_obj.is_a?(ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::SAPProfile)
+      flavor_obj.try(:cpus)
+    else
+      request.options[:entitled_processors].to_f.ceil
+    end
+  end
+
   def self.raw_import_image(ext_management_system, options = {})
     session_id = SecureRandom.uuid
     wrkfl_timeout = options['timeout'].to_i.hours
